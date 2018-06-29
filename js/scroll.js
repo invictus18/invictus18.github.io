42e20f34f51c722569d87b4b1ad93e08c697baaa
Kunal Bhat <kunal.bhat2001@gmail.com>
2018-06-29 23:41:59 +0530

    Index
    Initial Commit

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+console.log('bounce')
+$("#scr").hover(function(){
+    $(this).css("background-color","black");
+    $('#scr_img').attr("src","asset/scroll_over.png");
+}, function(){
+    $(this).css("background-color","white");
+    $('#scr_img').attr("src","asset/scroll.png");
+});
+
+$('#scr').click(function(){
+    $(window).scrollTo(".container",1500, {
+        onAfter: function(){
+            requestAnimationFrame(function(){
+                $('.anim').addClass('.bounceIn');
+            })
+        }
+    }
+)});
