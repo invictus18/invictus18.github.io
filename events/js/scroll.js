137ed1976a2bfa402eac2520b5f660bee1f23a68
Kunal Bhat <kunal.bhat2001@gmail.com>
2018-06-30 13:49:11 +0530

    Fix Events on Mobile

---
+++ events/js/scroll.js
@@ -0,0 +1,22 @@
+$("#scr").hover(function(){
+    $(this).css("background-color","black");
+    $('#scr_img').attr("src","asset/scroll_over.png");
+}, function(){
+    $(this).css("background-color","white");
+    $('#scr_img').attr("src","asset/scroll.png");
+});
+
+$('#scr').click(function(){
+    $('.container').show();
+    $(window).scrollTo(".container",1500, {
+        onAfter: function(){
+            requestAnimationFrame(function(){
+                $('.container').css("visibility","visible");
+                $('.container').addClass('animated bounceIn');
+            })
+        }
+    })
+});
+   
+
+
